9f2ee857c49b15daa29ef689243560ffa27ab11b
Jung-YouHwan <yhwjd99@gmail.com>
2024-02-09 21:27:53 +0900

    feat : 주문 가격 계산, 할인 적용

--- src/main/java/domain_entity_separation/domain/order/Order.java
+++ src/main/java/domain_entity_separation/domain/order/Order.java
@@ -1,10 +1,29 @@
 package domain_entity_separation.domain.order;
 
 import domain_entity_separation.domain.basket.Basket;
+import domain_entity_separation.domain.member.Member;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public record Order(
+        Member member,
         List<Basket> baskets
 ) {
+
+    public long progressPayment(final LocalDate now) {
+        List<Basket> baskets = baskets();
+        long totalPrice = baskets.stream()
+                .mapToLong(basket -> basket.calculatePrice(now))
+                .sum();
+
+        validateMoney(totalPrice);
+        return member.money() - totalPrice;
+    }
+
+    private void validateMoney(final long totalPrice) {
+        if (member.money() < totalPrice) {
+            throw new IllegalArgumentException("금액이 부족합니다.");
+        }
+    }
 }
