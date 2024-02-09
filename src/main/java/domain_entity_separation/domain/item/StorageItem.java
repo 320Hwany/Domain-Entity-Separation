d81d311cb8d6ba72dbe877db043c3019510fb0bc
Jung-YouHwan <yhwjd99@gmail.com>
2024-02-09 21:38:53 +0900

    feat : 재고 수량 계산

--- src/main/java/domain_entity_separation/domain/item/StorageItem.java
+++ src/main/java/domain_entity_separation/domain/item/StorageItem.java
@@ -1,7 +1,19 @@
 package domain_entity_separation.domain.item;
 
 public record StorageItem(
-        Item item,
         long totalQuantity
 ) {
+
+    public long calculateQuantity(final long itemQuantity) {
+        long remainQuantity = totalQuantity - itemQuantity;
+        validateQuantity(remainQuantity);
+
+        return remainQuantity;
+    }
+
+    private void validateQuantity(final long remainQuantity) {
+        if (remainQuantity < 0) {
+            throw new IllegalArgumentException("재고 수량이 부족합니다");
+        }
+    }
 }
