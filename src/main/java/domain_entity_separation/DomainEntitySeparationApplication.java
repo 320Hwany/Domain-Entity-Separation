fc6f134f58df4f0f65734e4cdd208080843d43e4
Jung-YouHwan <yhwjd99@gmail.com>
2024-02-09 17:06:01 +0900

    feat : 초기 설정

---
+++ src/main/java/domain_entity_separation/DomainEntitySeparationApplication.java
@@ -0,0 +1,13 @@
+package domain_entity_separation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainEntitySeparationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainEntitySeparationApplication.class, args);
+    }
+
+}
